02659ee8b7ed2925a881363d2b09e96f136e5bf6
Fabian Marz <fabian@netzstrategen.com>
2017-07-28 14:32:18 +0200

    Added override for twig.include tag.

--- src/tags/index.js
+++ src/tags/index.js
@@ -3,6 +3,90 @@
 module.exports = function(fractal){
 
     return {
+        include(Twig) {
+            return {
+                /**
+                 * Block logic tokens.
+                 *
+                 *  Format: {% includes "template.twig" [with {some: 'values'} only] %}
+                 */
+                type: Twig.logic.type.include,
+                regex: /^include\s+(ignore missing\s+)?(.+?)\s*(?:with\s+([\S\s]+?))?\s*(only)?$/,
+                next: [],
+                open: true,
+                compile: function (token) {
+                    var match = token.match,
+                      includeMissing = match[1] !== undefined,
+                      expression = match[2].trim(),
+                      withContext = match[3],
+                      only = ((match[4] !== undefined) && match[4].length);
+
+                    delete token.match;
+
+                    token.only = only;
+                    token.includeMissing = includeMissing;
+
+                    token.stack = Twig.expression.compile.apply(this, [
+                        {
+                            type: Twig.expression.type.expression,
+                            value: expression
+                        }
+                    ]).stack;
+
+                    if (withContext !== undefined) {
+                        token.withStack = Twig.expression.compile.apply(this, [
+                            {
+                                type: Twig.expression.type.expression,
+                                value: withContext.trim()
+                            }
+                        ]).stack;
+                    }
+
+                    return token;
+                },
+                parse: function (token, context, chain) {
+                    // Resolve filename
+                    var innerContext = {},
+                      withContext,
+                      i,
+                      template;
+
+                    if (!token.only) {
+                        innerContext = Twig.ChildContext(context);
+                    }
+
+                    if (token.withStack !== undefined) {
+                        withContext = Twig.expression.parse.apply(this, [
+                            token.withStack,
+                            context
+                        ]);
+
+                        for (i in withContext) {
+                            if (withContext.hasOwnProperty(i))
+                                innerContext[i] = withContext[i];
+                        }
+                    }
+
+                    var file = Twig.expression.parse.apply(this, [
+                        token.stack,
+                        innerContext
+                    ]);
+
+                    if (file instanceof Twig.Template) {
+                        template = file;
+                    }
+                    else {
+                        // Import file
+                        template = this.importFile(file);
+                    }
+
+                    return {
+                        chain: chain,
+                        output: template.render(innerContext)
+                    };
+                }
+            }
+        },
         trans(Twig) {
             return {
                 type: 'trans',
