53cbe96d7d131c8bb405c2c33274da3d11cff4b4
Julià Mestieri <juli@netzstrategen.com>
2017-10-06 19:28:02 +0200

    Added support for the Drupal filter 'without'.

--- src/filters/index.js
+++ src/filters/index.js
@@ -8,6 +8,14 @@ module.exports = function (fractal) {
         field_value(str) {
             return str;
         },
+        without(element, exclude) {
+            for (var i = 0; i < exclude.length; i++) {
+                if (element.hasOwnProperty(exclude[i])) {
+                    delete element[exclude[i]];
+                }
+            }
+            return element;
+        },
         path: require('./path.js')(fractal),
     }
 };
