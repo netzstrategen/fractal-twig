79fad3813071e8709626288fcb8f9670a9607472
Julià Mestieri <juli@netzstrategen.com>
2017-10-09 17:24:50 +0200

    Added filter 'without' from Drupal. (#3)

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const _ = require('lodash');
 
 module.exports = function (fractal) {
     return {
@@ -7,6 +8,15 @@ module.exports = function (fractal) {
         },
         field_value(str) {
             return str;
+        },
+        without(element, exclude_elements) {
+            var filtered_element = _.cloneDeep(element);
+            exclude_elements.forEach(function (exclude) {
+                if (element.hasOwnProperty(exclude)) {
+                    delete filtered_element[exclude];
+                }
+            });
+            return filtered_element;
         },
         path: require('./path.js')(fractal),
     }
